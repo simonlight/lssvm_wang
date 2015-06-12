4a26f026070cc43fee6281b3ba16713d098aad9c
xin <simonlight90@gmail.com>
2015-06-12 03:02:58 +0200

    LSSVMMulticlassTestET_negative_positive uses gaze-enabled prediction function/only positive pred

--- batch_run.sh
+++ batch_run.sh
@@ -8,13 +8,13 @@ scale_arr=("100" "90" "80" "70" "60")
 #scale_arr=("100" "90" "80" "70" "60" "50")
 #cls_arr=("dog")
 #scale_arr=("100")
-k='oarsub -p "host='"'"'big7'"'"'  " -l "nodes=1/core=1,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET_negative_positive'
+k='oarsub -p "host='"'"'big7'"'"'  " -l "nodes=1/core=2,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET_negative_positive'
 end='"'
 space=' '
-for cls in ${cls_arr[@]}
+for scale in ${scale_arr[@]}
 do
-        for scale in ${scale_arr[@]}
-        do
-                eval $k$space$cls$space$scale$end
-        done
+	for cls in ${cls_arr[@]}
+    do
+        eval $k$space$cls$space$scale$end
+    done
 done
