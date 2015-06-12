f6ee5a8ff1e2e55c9eb78c573361685f343f5f68
xin <simonlight90@gmail.com>
2015-06-12 03:15:10 +0200

    LSSVMMulticlassTestET_negative_positive uses no prediction function/only positive pred

--- batch_run.sh
+++ batch_run.sh
@@ -8,7 +8,7 @@ scale_arr=("100" "90" "80" "70" "60")
 #scale_arr=("100" "90" "80" "70" "60" "50")
 #cls_arr=("dog")
 #scale_arr=("100")
-k='oarsub -p "host='"'"'big7'"'"' or host='"'"'big13'"'"' " -l "nodes=1/core=2,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/test/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET_negative_positive'
+k='oarsub -p "host='"'"'big7'"'"' " -l "nodes=1/core=2,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/test/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET_negative_positive'
 end='"'
 space=' '
 for scale in ${scale_arr[@]}
