d0c51fc5859450e1244323406d9ec33e6b6ac7f9
xin <simonlight90@gmail.com>
2015-06-14 16:51:55 +0200

    new pred

--- read_metric_file.py
+++ read_metric_file.py
@@ -8,9 +8,9 @@ def get_best_cv(cls, scale):
         if c.strip()== cls and s.strip() == str(scale):
             return best_cv
 
-root="/home/wangxin/Data/ferrari_data/reduit_allbb/results_neg_pos_no_prediction/"
+root="/home/wangxin/Data/ferrari_data/reduit_allbb/results_neg_pos_new_prediction/"
 # classes=['cat', 'dog', 'bicycle', 'motorbike', 'boat', 'aeroplane', 'horse', 'cow', 'sofa', 'diningtable']
-classes=['cat', 'dog', 'boat', 'aeroplane', 'horse', 'cow', 'sofa', 'diningtable']
+classes=['cat', 'dog', 'boat','bicycle', 'aeroplane', 'horse', 'cow', 'sofa', 'diningtable']
 #classes = ['boat']
 scales=[100,90,80,70,60,50]
 for cls in classes:
