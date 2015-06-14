4e96c3b15f23be4613468d8c61bc73e90fd88176
xin <simonlight90@gmail.com>
2015-06-14 16:38:46 +0200

    only evaluation task branch

--- data_cv_select.py
+++ data_cv_select.py
@@ -41,11 +41,11 @@ def best_res(p):
 #             print k
             
 if __name__=='__main__':
-    new_pred = "/local/wangxin/res_neg_pos_new_prediction.txt"
-    neg_pos_res_path = "/local/wangxin/res_neg_pos_no_prediction.txt"
-    std_et_res_path = "/local/wangxin/std_et_no_prediction.txt"
+    new_pred = "/home/wangxin/Data/ferrari_data/reduit_allbb/res_neg_pos_new_prediction.txt"
+    neg_pos_res_path = "/home/wangxin/Data/ferrari_data/reduit_allbb/res_neg_pos_no_prediction.txt"
+    std_et_res_path = "/home/wangxin/Data/ferrari_data/reduit_allbb/std_et_no_prediction.txt"
     
-    #statistic(std_et_res_path)  
+    statistic(new_pred)  
 #     print "###############"
-    best_res(std_et_res_path)
+#    best_res(std_et_res_path)
 #     best_res(neg_pos_res_path)  
