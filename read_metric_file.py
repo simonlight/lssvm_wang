424bf0b0ad1dfe724d0c7801ea26afe54762d0c2
xin <simonlight90@gmail.com>
2015-06-14 11:56:50 +0200

    calculate accuracy from metric file

--- read_metric_file.py
+++ read_metric_file.py
@@ -14,7 +14,6 @@ classes=['cat', 'dog', 'boat', 'aeroplane', 'horse', 'cow', 'sofa', 'diningtable
 #classes = ['boat']
 scales=[100,90,80,70,60,50]
 for cls in classes:
-    
     for scale in scales:        
         best_cv = get_best_cv(cls, scale)
         f= open(root+"metric_"+str(scale)+"_"+cls+"_"+str(best_cv)+"_pos_neg.txt")
@@ -25,4 +24,4 @@ for cls in classes:
             yp, h, image_path = line.strip().split(',')
             if yp == '1':
                 correct += 1
-        print correct/cnt
+        print cls, scale, correct/cnt
