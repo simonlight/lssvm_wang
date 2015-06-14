3370e66c3755befee50f0e3c08d57eed15118d03
xin <simonlight90@gmail.com>
2015-06-14 16:43:19 +0200

    t

--- data_cv_select.py
+++ data_cv_select.py
@@ -45,7 +45,7 @@ if __name__=='__main__':
     neg_pos_res_path = "/home/wangxin/Data/ferrari_data/reduit_allbb/res_neg_pos_no_prediction.txt"
     std_et_res_path = "/home/wangxin/Data/ferrari_data/reduit_allbb/std_et_no_prediction.txt"
     
-    statistic(new_pred)  
+    statistic(neg_pos_res_path)  
 #     print "###############"
 #    best_res(std_et_res_path)
 #     best_res(neg_pos_res_path)  
