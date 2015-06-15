a1deb56f8e4b8626d0b671995c51de697e0fed56
xin_urbandancesquad <xin.wang@lip6.fr>
2015-06-15 13:34:52 +0200

    5 class 4 cpus

--- batch_run.sh
+++ batch_run.sh
@@ -11,7 +11,7 @@ cls_arr=("aeroplane" "horse" "cow" "sofa" "diningtable" )
 scale_arr=("100" "90" "80" "70" "60" "50")
 #cls_arr=("bicycle")
 #scale_arr=("100")
-k='oarsub -p "host='"'"'big15'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET_negative_positive'
+k='oarsub -p "host='"'"'big9'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET_negative_positive'
 end='"'
 space=' '
 for scale in ${scale_arr[@]}
