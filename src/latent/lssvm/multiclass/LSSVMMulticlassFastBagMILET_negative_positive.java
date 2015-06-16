ae31408021afe6aeb09eff6c6e468747eb702fdc
xin_urbandancesquad <xin.wang@lip6.fr>
2015-06-16 11:34:53 +0200

    metric write opt

--- src/latent/lssvm/multiclass/LSSVMMulticlassFastBagMILET_negative_positive.java
+++ src/latent/lssvm/multiclass/LSSVMMulticlassFastBagMILET_negative_positive.java
@@ -151,28 +151,27 @@ public class LSSVMMulticlassFastBagMILET_negative_positive extends LSSVMMulticla
 	public double testAPRegion(List<STrainingSample<LatentRepresentation<BagMIL, Integer>, Integer>> l, int scale, String simDir, String className, double tradeoff) {
 		
 		List<Evaluation<Integer>> eval = new ArrayList<Evaluation<Integer>>();
-		for(int i=0; i<l.size(); i++) {
-        	// calcul score(x,y,h,w) = argmax_{y,h} <w, \psi(x,y,h)>
-        	Integer y = prediction(l.get(i).input);
-        	Integer h = prediction(l.get(i).input.x, y);
-        		File resFile=new File(simDir+"results_neg_pos_no_prediction/metric_"+String.valueOf(scale)+"_"+className+"_"+String.valueOf(tradeoff)+"_"+"pos_neg"+".txt");
-        		resFile.getAbsoluteFile().getParentFile().mkdirs();
-        		try {
-        			BufferedWriter out = new BufferedWriter(new FileWriter(resFile, true));
-        			out.write(Integer.valueOf(y) +","+ Integer.valueOf(h)+","+l.get(i).input.x.getName()+"\n");
-        			out.flush();
-        			out.close();
-        			
-        		} catch (IOException e) {
-        			// TODO Auto-generated catch block
-        			e.printStackTrace();
-        		}
-        	
-        	double score = valueOf(l.get(i).input.x,y,h,w);
-                
-        	eval.add(new Evaluation<Integer>((l.get(i).output == 0 ? -1 : 1), (y == 0 ? -1 : 1)*score));
-                //System.out.println(l.get(i).label + "\t" + scores[i] + ";");
-        }
+		File resFile=new File(simDir+"results_neg_pos_no_prediction_test/metric_"+String.valueOf(scale)+"_"+className+"_"+String.valueOf(tradeoff)+"_"+"pos_neg"+".txt");
+		resFile.getAbsoluteFile().getParentFile().mkdirs();
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(resFile));
+			for(int i=0; i<l.size(); i++) {
+	        	// calcul score(x,y,h,w) = argmax_{y,h} <w, \psi(x,y,h)>
+	        	Integer y = prediction(l.get(i).input);
+	        	Integer h = prediction(l.get(i).input.x, y);	
+				out.write(Integer.valueOf(y) +","+ Integer.valueOf(h)+","+l.get(i).input.x.getName()+"\n");
+				out.flush();
+	        	double score = valueOf(l.get(i).input.x,y,h,w);
+	        	eval.add(new Evaluation<Integer>((l.get(i).output == 0 ? -1 : 1), (y == 0 ? -1 : 1)*score));
+	                //System.out.println(l.get(i).label + "\t" + scores[i] + ";");
+	        }
+			out.close();
+	        	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
         double ap = AveragePrecision.getAP(eval);
         return ap;
 	}
