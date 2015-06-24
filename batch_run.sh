869f1421c07846cb8a98e9076d551cf776f73461
xin_urbandancesquad <xin.wang@lip6.fr>
2015-06-24 15:47:26 +0200

    batch run

--- batch_run.sh
+++ batch_run.sh
@@ -1,7 +1,7 @@
 #cls_arr=("dog" "cat" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "diningtable")
 
-cls_arr=("dog" "cat" "bicycle" "motorbike" "boat")
-#cls_arr=("aeroplane" "horse" "cow" "sofa" "diningtable" )
+cls_arr=("dog" "cat" "bicycle" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "diningtable")
+#cls_arr=("aeroplane" "horse" "cow" "sofa" "diningtable")
 #cls_arr=("horse" "cow" "sofa" "diningtable" "aeroplane")
 #scale_arr=("100" "90" "80")
 #cls_arr=("motorbike" "boat")
@@ -11,7 +11,7 @@ cls_arr=("dog" "cat" "bicycle" "motorbike" "boat")
 scale_arr=("100" "90" "80" "70" "60" "50")
 #cls_arr=("bicycle")
 #scale_arr=("100")
-k='oarsub -p "host='"'"'big12'"'"' or host='"'"'big15'"'"'" -l "nodes=1/core=6,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET'
+k='oarsub -p "host='"'"'big15'"'"'" -l "nodes=1/core=6,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. data/uiuc/mac/LSSVMMulticlassTestET'
 end='"'
 space=' '
 for scale in ${scale_arr[@]}
