daa3f7f54581d2cfe449cef08e60ec5cab6bf45b
xin_urbandancesquad <xin.wang@lip6.fr>
2015-06-25 10:35:49 +0200

    std lssvm

--- batch_run.sh
+++ batch_run.sh
@@ -1,6 +1,5 @@
-#cls_arr=("dog" "cat" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "diningtable")
-
 cls_arr=("dog" "cat" "bicycle" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "diningtable")
+#cls_arr=("dog" "cat" "bicycle" "motorbike" "boat" )
 #cls_arr=("aeroplane" "horse" "cow" "sofa" "diningtable" )
 #cls_arr=("horse" "cow" "sofa" "diningtable" "aeroplane")
 #scale_arr=("100" "90" "80")
