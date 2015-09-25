9ba4f2d784f0ab6ed23c650589fc3530f25fdc4b
xin_urbandancesquad <xin.wang@lip6.fr>
2015-09-25 19:28:51 +0200

    debug: is ferrari ok?

--- src/data/uiuc/mac/LSSVMMulticlassTest.java
+++ src/data/uiuc/mac/LSSVMMulticlassTest.java
@@ -150,7 +150,7 @@ public class LSSVMMulticlassTest {
         	System.exit(-1);
 	    }
 		
-	    double[] lambdaCV = {1};//1e-4
+	    double[] lambdaCV = {1e-1};//1e-4
 	    double[] epsilonCV = {1e-2};//1e-2
 	    
 
