1a4171d1d641fc9d028b8a33a2a887f63a6dc085
xin_urbandancesquad <xin.wang@lip6.fr>
2015-09-28 17:01:58 +0200

    ffffff

--- src/data/uiuc/mac/LSSVMMulticlassTestET.java
+++ src/data/uiuc/mac/LSSVMMulticlassTestET.java
@@ -238,8 +238,9 @@ public class LSSVMMulticlassTestET {
 			    			//if(compute || fileClassifier == null) {
 			    			if(true){
 			    				lsvm.train(exampleTrain);
-			    				double ap_train = lsvm.testAP(exampleTrain);
-								System.err.println("train " + String.valueOf(tradeoff)+" "+cls + " scale= " + scale + " ap= " + ap_train + " lambda= " + lambda + " epsilon= " + epsilon);
+			    				double ap_train = lsvm.testAPRegion(exampleTrain, epsilon, lambda,scale, simDir, className, tradeoff);
+			    				
+			    				System.err.println("train " + String.valueOf(tradeoff)+" "+cls + " scale= " + scale + " ap= " + ap_train + " lambda= " + lambda + " epsilon= " + epsilon);
 								
 								//double acc = lsvm.test(exampleTrain);
 								//System.err.println("train - " + cls + "\tscale= " + scale + "\tacc= " + acc + "\tlambda= " + lambda + "\tepsilon= " + epsilon);
