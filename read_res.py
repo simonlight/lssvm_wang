eb68025a8056cd2cadb4839b1f4878a1ad65b27a
xin_urbandancesquad <xin.wang@lip6.fr>
2015-09-29 18:58:27 +0200

    read results

--- read_res.py
+++ read_res.py
@@ -20,7 +20,7 @@ et_res = read_res(et)
 
 print std_res.values()
 
-stdsum=0
+# stdsum=0
 """for k,v in res_std.items():
 	print k,
 	v_max=0
@@ -48,26 +48,26 @@ for k,v in res_et.items():
 	
 
 	
-etsum=0
-for k,v in res_std.items():
-	print k+'\t',
-	class_avg=0.0
-	v_max=0
-	k_max=0
-	min_c=10000
-	max_c=0
-	c=0
-	for k_,v_ in v.items():
-		c+=1
-		etsum+=float(v_)
-		class_avg+=float(v_)
-		if float(v_)< min_c:
-			min_c =float(v_)
-		if float(v_)> max_c:
-			max_c =float(v_)
-			
-	print  str(class_avg/c)+'\t',
-	print str(max_c)+'\t',
-	print str(min_c)+'\n',
-print stdsum/61
-print etsum/61
+# etsum=0
+# for k,v in res_std.items():
+# 	print k+'\t',
+# 	class_avg=0.0
+# 	v_max=0
+# 	k_max=0
+# 	min_c=10000
+# 	max_c=0
+# 	c=0
+# 	for k_,v_ in v.items():
+# 		c+=1
+# 		etsum+=float(v_)
+# 		class_avg+=float(v_)
+# 		if float(v_)< min_c:
+# 			min_c =float(v_)
+# 		if float(v_)> max_c:
+# 			max_c =float(v_)
+# 			
+# 	print  str(class_avg/c)+'\t',
+# 	print str(max_c)+'\t',
+# 	print str(min_c)+'\n',
+# print stdsum/61
+# print etsum/61
