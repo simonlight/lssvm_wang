8273559da6fdae046190c3b2c0011859719d956a
xin_urbandancesquad <xin.wang@lip6.fr>
2015-10-14 18:36:00 +0200

    ferrari complete

--- batch_run.sh
+++ batch_run.sh
@@ -4,7 +4,7 @@
 
 #scale_arr=("100" "90" "80" "70" "60" "50" "40" "30")
 #cls_arr=("reading")
-cls_arr=("dog")
+cls_arr=("dog" "cat")
 
 scale_arr=("50" "40")
 k='oarsub -p "host='"'"'big$1'"'"' " -l "nodes=1/core=2,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. 
