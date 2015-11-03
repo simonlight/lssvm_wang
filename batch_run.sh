e456e83ef4beab5fc6f206a2efae6f2be288bac9
xin_urbandancesquad <xin.wang@lip6.fr>
2015-11-03 20:10:22 +0100

    ferrari scale 50 cv gamma

--- batch_run.sh
+++ batch_run.sh
@@ -7,7 +7,7 @@ cls_arr=("dog" "cat" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "dining
 #cls_arr=("phoning")
 #cls_arr=("horse")
 
-scale_arr=("40")
+scale_arr=("50")
 k='oarsub -p "host='"'"'big$1'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. 
 data/uiuc/mac/LSSVMMulticlassTestET'
 
