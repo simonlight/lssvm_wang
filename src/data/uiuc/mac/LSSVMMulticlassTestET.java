6f882584ab158fc652cb339fff3720af4646e144
wangxin <simonlight90@gmail.com>
2015-11-25 13:12:53 +0100

    60 dog

--- src/data/uiuc/mac/LSSVMMulticlassTestET.java
+++ src/data/uiuc/mac/LSSVMMulticlassTestET.java
@@ -26,7 +26,7 @@ public class LSSVMMulticlassTestET {
 	
 	public static void main(String[] args) {
 		
-		String dataSource= "local";//local or other things
+		String dataSource= "big";//local or other things
 		String gazeType = "ferrari";
 
 		String sourceDir = new String();
